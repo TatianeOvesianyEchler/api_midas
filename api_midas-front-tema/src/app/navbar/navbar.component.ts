e381c53078be1e3e414e30f66574d5f617e459fc
Juliana <juliana.hansen@engenharia.com.br>
2022-08-17 06:27:48 -0300

    autenticação

--- api_midas-front-tema/src/app/navbar/navbar.component.ts
+++ api_midas-front-tema/src/app/navbar/navbar.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuarioService } from '../autenticacao/usuario/usuario.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
 
-  constructor() { }
+  user$ = this.usuarioService.retornaUsuario();
+
+  constructor(private usuarioService: UsuarioService, private router: Router) { }
+
+  logout() {
+    this.usuarioService.logout();
+    this.router.navigate(['']);
+  }
 
   ngOnInit(): void {
   }
